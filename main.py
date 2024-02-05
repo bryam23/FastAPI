ad618f301c3087926eda4cc957ca9ec7ae4b9542
Bryam <bryamguerrero6@gmail.com>
2024-02-05 11:49:09 -0500

    ajustes

--- main.py
+++ main.py
@@ -49,3 +49,55 @@ def userdata(user_id:str):
     # calculamos la cantidad de dinero que el usuario ha gastado en items
     cantidad_dinero= round((df_filtrado['price'].sum()), 4)
     return {f'usuario':user_id, 'dinero gastado':f'{cantidad_dinero} USD', 'porcentaje de recomendacion':f'{porcentaje_recomendacion}%', 'cantidad de items':total_items}
+
+
+
+# tercera funcion
+@app.get('/UserForGenre/{genero}')
+def UserForGenre(genero:str):
+  # filtramos dataframe para obtener la informacion del genero especificado
+  generos = funcion3[funcion3['genres'].str.contains(genero)]
+
+  # encontramos al usuario con mas horas jugadas aplicando funcion lambda
+  usuario_max_horas = generos.assign(
+    total_horas=lambda x: x['playtime_forever'].groupby(x['user_id']).transform('sum')
+  ).sort_values('total_horas', ascending=False).iloc[0]['user_id']
+
+  # Obtenemos las horas jugadas por año
+  horas_por_año = generos.groupby('release_year')['playtime_forever'].sum().reset_index(name='Horas')
+  horas_por_año= horas_por_año.rename(columns={'release_year':'Año'})
+  # Convertir a una lista de diccionarios
+  horas_lista = horas_por_año.to_dict('records')
+
+  
+  return {
+    "Usuario con más horas jugadas para Género " + genero: usuario_max_horas,
+    "Horas jugadas": horas_lista
+  }
+
+
+
+# cuarta funcion
+@app.get('/best_developer_year/{year}')
+def best_developer_year(year: int):
+    # filtramos dataframe para obtener solo las recomendaciones del año dado
+    df_filtrado= funcion4[(funcion4['release_year']== year) & (funcion4['recommend']==True)]
+    # contamos las recomendaciones por desarrollador
+    conteo_recomendaciones= df_filtrado['developer'].value_counts()
+    # obtenemos top 3 de los desarrolladores
+    top_3_best_developers= conteo_recomendaciones.nlargest(3).index.tolist()
+    return [{'Puesto 1':top_3_best_developers[0], 'Puesto 2':top_3_best_developers[1], 'Puesto 3':top_3_best_developers[2]}]
+best_developer_year(2011)
+
+
+
+# quinta funcion
+@app.get('/developer_reviews_analysis/{desarrolladora}')
+def developer_reviews_analysis(desarrolladora:str):
+    # filtramos dataframe para obtener solo la informacion del desarrollador dado
+    df_filtrado= funcion5[funcion5['developer']==desarrolladora]
+    # contamos la cantidad de reseñas para cada tipo de sentimiento 
+    sentiment_count = df_filtrado['sentiment_analysis'].value_counts().to_dict()
+    # resultado a devolver
+    resultado= {desarrolladora: ['Negativo: ' + str(sentiment_count.get(0,0)), 'Positivo:' + str(sentiment_count.get(2,0))]}
+    return resultado
